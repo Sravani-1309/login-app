9460f512539dd99f3a0eacfcdab3f893d91b4217
Sravani Reddy <kistapatisravanireddy@gmail.com>
2026-03-05 12:15:01 +0530

    updated server port for render

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
